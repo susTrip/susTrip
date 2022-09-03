a83b55d479556817383af0da5fffda9ed9a0b8ec
Andrew Mascillaro <amascillaro@gmail.com>
2022-09-03 10:57:17 -0400

    Create initial map

---
+++ backend/pages/map.jsx
@@ -0,0 +1,28 @@
+// var mapboxgl = require('mapbox-gl');
+// var MapboxDirections = require('@mapbox/mapbox-gl-directions');
+
+
+
+
+const Map = () => {
+    // var directions = new MapboxDirections({
+    //     accessToken: 'pk.eyJ1Ijoic3VzdHJpcCIsImEiOiJjbDdtMDMzdHUwOXd2M3ZwOG9hN29heXV5In0.Y3_7dFxQF5xjS7WuhtdxiQ',
+    //     unit: 'imperial',
+    //     exclude: 'mapbox/driving',
+    //     profile: 'mapbox/driving-traffic',
+    //     alternatives: true
+    // });
+        
+    // var map = new mapboxgl.Map({
+    //     container: 'map',
+    //     style: 'mapbox://styles/mapbox/streets-v9'
+    // });
+
+    // map.addControl(directions, 'top-left');
+
+    return (
+        <div>Hello test</div>
+    )
+}
+
+export default Map
